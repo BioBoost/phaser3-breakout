acd87f8ac1f6434addd589f93b3660f048b1c4fb
Nico De Witte <nico.dewitte@outlook.com>
2019-03-01 12:11:40 +0100

    Add ball image

--- game.js
+++ game.js
@@ -16,14 +16,22 @@ var config = {
   }
 };
 
-var game = new Phaser.Game(config);
+let game = new Phaser.Game(config);
+let balls;
 
 function preload () {
   this.load.image('background', 'assets/backgrounds/space.jpg');
+  this.load.image('ball', 'assets/ball.png');
 }
 
 function create () {
   this.add.image(0, 0, 'background').setOrigin(0, 0);
+
+  // Create the list of balls as a dynamic physics body group
+  balls = this.physics.add.group({
+    key: 'ball',
+    setXY: { x: 640, y: 500 }
+  });
 }
 
 function update () {

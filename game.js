24266d4c3db52e4b1efb2d8799c20cb1c06593d5
Nico De Witte <nico.dewitte@outlook.com>
2019-03-01 11:52:10 +0100

    Make game window larger

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 1280,
+  height: 800,
   scene: {
       preload: preload,
       create: create,
@@ -12,6 +12,7 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload () {
+  this.load.image('sky', 'assets/sky.png');
 }
 
 function create () {
